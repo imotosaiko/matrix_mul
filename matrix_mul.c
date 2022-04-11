18505529fa8e39a6d51a3268c33c660be541e8be
nannanko <nannanko@kazv.moe>
2022-04-11 19:43:38 +0800

    loop split once

--- matrix_mul.c
+++ matrix_mul.c
@@ -28,17 +28,33 @@ void matrix_mul(matrix_t matrix_r,              \
                 matrix_t matrix2,               \
                 dim_t m, dim_t n, dim_t k) {
     for (dim_t i1 = 0; i1 < m; i1++) {
-        for (dim_t i2 = 0; i2 < k; i2++) {
+        for (dim_t i2 = 0; i2 < k; i2 += 4) {
             /* malloc initliaze memory to 0 by default
              *
              * C[i1][i2] = 0
+             * C[i1][i2 + 1] = 0
+             * C[i1][i2 + 2] = 0
+             * C[i1][i2 + 3] = 0
              */
             for (dim_t i3 = 0; i3 < n; i3++) {
                 // C[i1][i2] += A[i1][i3] * B[i3][i2]
                 matrix_r[i1 * k + i2] +=    \
                     matrix1[i1 * n + i3] *  \
                     matrix2[i3 * k + i2];
+                // C[i1][i2 + 1] += A[i1][i3] * B[i3][i2 + 1]
+                matrix_r[i1 * k + i2 + 1] +=    \
+                    matrix1[i1 * n + i3] *      \
+                    matrix2[i3 * k + i2 + 1];
+                // C[i1][i2 + 2] += A[i1][i3] * B[i3][i2 + 2]
+                matrix_r[i1 * k + i2 + 2] +=    \
+                    matrix1[i1 * n + i3] *      \
+                    matrix2[i3 * k + i2 + 2];
+                // C[i1][i2 + 3] += A[i1][i3] * B[i3][i2 + 3]
+                matrix_r[i1 * k + i2 + 3] +=    \
+                    matrix1[i1 * n + i3] *      \
+                    matrix2[i3 * k + i2 + 3];
             }
         }
     }
 }
+
